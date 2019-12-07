3668f4fab2d4c8c629a069e1c8b0e995d6e6090b
Vita <vita.petrovskaya@gmail.com>
2019-12-07 19:08:45 +0300

    Add filedialog

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,12 @@
 import java.awt.EventQueue;
+import java.awt.FileDialog;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Parser {
 
@@ -34,8 +40,42 @@ public class Parser {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 995, 538);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menubar = new JMenuBar();
+		JMenu file = new JMenu("File");
+		
+		JMenuItem open = new JMenuItem("Open");
+		open.addActionListener(new ActionListener() {
+			 @Override
+             public void actionPerformed(ActionEvent arg0) {
+				 FileDialog fd = new FileDialog(frame, "Choose a file", FileDialog.LOAD);
+				 fd.setDirectory("C:\\VHDL_examples");
+				 fd.setFile("*.vhd");
+				 fd.setVisible(true);
+				 String filename = fd.getFile();
+				 if (filename == null)
+				   System.out.println("You cancelled the choice");
+				 else
+				   System.out.println("You chose " + filename);
+             }
+		});
+		file.add(open);
+		
+		JMenuItem save = new JMenuItem("Save");
+		file.add(save);
+		
+		JMenuItem exit = new JMenuItem("Exit");
+		exit.addActionListener(new ActionListener() {
+			 @Override
+            public void actionPerformed(ActionEvent arg0) {
+                System.exit(1);
+            }
+		});
+		file.add(exit);
+		
+		menubar.add(file);
+		frame.setJMenuBar(menubar);
 	}
-
 }
